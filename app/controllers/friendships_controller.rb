eaca6f1e722a3c996ca69e2c2dc3b3301b49acab
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-07 18:11:45 +0300

    Build create action to build a new request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,18 @@
 class FriendshipsController < ApplicationController
+
+  def index
+    @friendships = Friendship.all
+  end
+
+  def new
+    @friendship = Friendship.new
+  end
+
+  def create
+    @friendship = current_user.pending_friendships.build(friend_id: params[:friend_id])
+    if @friendship.save
+      redirect_to users_path, notice: "Request sent!"
+    else
+      redirect_to users_path, notice: "Request failed!"
+  end
 end
