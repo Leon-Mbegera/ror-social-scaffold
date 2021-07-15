24a3de7b996c23b9ca5250e8a89ffefc9dafd443
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-15 17:29:58 +0300

    Fix rubocop offenses

--- spec/integration_test.rb
+++ spec/integration_test.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'user signing in and viewing timeline' do
   background do
-    user = User.new(name: 'leon', email: 'leon@example.com', password: '123456',)
+    user = User.new(name: 'leon', email: 'leon@example.com', password: '123456')
     visit new_user_registration_path
     fill_in 'user_name', with: user.name
     fill_in 'user_email', with: user.email
@@ -11,9 +11,8 @@ feature 'user signing in and viewing timeline' do
     click_button 'Sign up'
   end
 
- 
   scenario 'User view post page' do
     visit posts_path
     expect(page).to have_content('Recent posts')
-  end 
+  end
 end
--- spec/models_tests/like_spec.rb
+++ spec/models_tests/like_spec.rb
@@ -5,6 +5,5 @@ RSpec.describe Like, type: :model do
     user = User.create(name: 'Leon', email: 'leon@example.com', password: '123456')
     first_post = Post.create(content: 'Bonnie and Clyde, a legendary criminal couple', user: user)
     expect(Like.create(user: user, post: first_post)).to be_valid
-    
   end
 end
