2ccd68916863ed0a7c8ee5b2df5c6030b0689e77
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-16 12:44:27 +0300

    Mutual confirmed friendships associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,19 @@ class User < ApplicationRecord
   has_many :friendships # sent requests
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id' # received requests
 
-  has_many :pending_friendships, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friendships, -> {where (confirmed: false)}, class_name: 'Friendship', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
+  has_many :accepted_friendships_thru_pending_friendships, -> {where (confirmed: true)}, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :accepted_friends_thru_pending_friendships, through: :accepted_friendships_thru_pending_friendships, source: :user
+
+  has_many :inverse_friendships, -> {where (confirmed: false)}, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  has_many :accepted_friendships_thru_inverse_friendships, -> {where (confirmed: true)}, class_name: 'Friendship',  
+  foreign_key: 'user_id'
+  has_many :accepted_friends_thru_inverse_friendships, through: :accepted_friendships_thru_inverse_friendships, source: :friend
+  
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
     friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
